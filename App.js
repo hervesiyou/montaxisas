770b2eee0df6ec4ad123945ad1cd1e53322df302
Edy Michel TCHOKOUANI <edmichs@MacBook-Air-de-Edy.local>
2019-04-16 18:26:59 +0100

    edy@retouch_splashscreen_anotherone

--- App.js
+++ App.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { Platform, StatusBar, StyleSheet, View, Image } from 'react-native';
 import { AppLoading, Asset, Font, Icon, SplashScreen } from 'expo';
 import AppNavigator from './navigation/AppNavigator';
-import JoinScreen  from "./screens/JoinScreen";
+import JoinScreen from "./screens/JoinScreen";
+import AuthLoadingScreen from './screens/AuthLoadingScreen';
 
 
 export default class App extends React.Component {
@@ -13,46 +14,48 @@ export default class App extends React.Component {
   componentDidMount() {
     setTimeout(() => {
       SplashScreen.preventAutoHide();
-        }, 5000);
-      
+    }, 5000);
+
   }
 
-  constructor(props){
+  constructor(props) {
     super(props)
     this.state = { isLoading: true }
   }
 
-  
+
 
   render() {
-        if (!this.state.isReady) {
-          return (
-            
-              <Image style={{ backgroundColor:"#FFF301"}}
-                source={require('./assets/images/Andriod_copie.png')}
-                resizeMode= 'center'
-                onLoad={this._cacheResourcesAsync}
-              />
-          );
-        
-        } 
+    if (!this.state.isReady) {
       return (
-        <View style={styles.container}>
-          {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
-          <JoinScreen />
+        <View style={styles.container}> 
+          <Image style={styles.image}
+            source={require('./assets/images/screen.png')}
+            resizeMode='contain'
+            onLoad={this._cacheResourcesAsync}
+          />
         </View>
+
       );
+
+    }
+    return (
+      <View style={styles.container}>
+        {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
+        <JoinScreen />
+      </View>
+    );
 
   }
 
 
   _cacheSplashResourcesAsync = async () => {
-    
-      const gif = require('./assets/images/Andriod_copie.png');
-      return Asset.fromModule(gif).downloadAsync();
-       
-      
-   
+
+    const gif = require('./assets/images/screen.png');
+    return Asset.fromModule(gif).downloadAsync();
+
+
+
   }
   _cacheResourcesAsync = async () => {
     SplashScreen.hide();
@@ -66,9 +69,10 @@ export default class App extends React.Component {
     });
 
     await Promise.all(cacheImages);
-    setTimeout(() => {this.setState({ isReady: true });
-        }, 5000);
-      
+    setTimeout(() => {
+      this.setState({ isReady: true });
+    }, 5000);
+
   }
 }
 
@@ -76,5 +80,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+  },
+  image:{
+   
   },
 });
